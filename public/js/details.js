09a2f8a333fb7674e24d5725526357b78525e0e6
iryna <musicira@gmail.com>
2024-01-09 13:29:27 +0100

    update style

---
+++ public/js/details.js
@@ -0,0 +1,25 @@
+$(function () {
+
+$("#equip_btn").on("click", function () {
+  $("#equip_btn").css("background", "#d94350");
+  $("#equip_btn").css("color", "white");
+  $(".equip").removeClass("hidden");
+});
+$("#caracter_btn").on("click", function () {
+  $("#caracter_btn").css("background", "#d94350");
+  $("#caracter_btn").css("color", "white");
+  $(".caract").removeClass("hidden");
+});
+$("#opt_btn").on("click", function () {
+  $("#opt_btn").css("background", "#d94350");
+  $("#opt_btn").css("color", "white");
+  $(".opt").removeClass("hidden");
+});
+
+$("#contact_btn").on("click", function () {
+  $("#contact_btn").css("background", "#d94350");
+  $("#contact_btn").css("color", "white");
+  $("#contactForm").removeClass("hidden");
+});
+});
+
