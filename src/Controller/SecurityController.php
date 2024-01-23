3409324afb16b7949d6a39eef1c45d37ce9f473e
iryna <musicira@gmail.com>
2024-01-23 13:15:38 +0100

    Auth update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,84 +2,37 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use App\Form\RegistrationType;
-use App\Repository\ScheduleRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ScheduleRepository;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route('/connexion', name: 'security.login', methods: ['GET', 'POST'])]
-    public function login(
-        AuthenticationUtils $authenticationUtils, 
-        ScheduleRepository $scheduleRepository
-        ): Response
-    { 
-        /**
-        * This controller allows us to login
-        *
-        * @param AuthenticationUtils $authenticationUtils
-        * @return Response
-        */
-   
-
-        return $this->render('pages/security/login.html.twig', [
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error' => $authenticationUtils->getLastAuthenticationError(),
-            'schedules' => $scheduleRepository->findAll()
-        ]);
-    }
-
-     /**
-     * This controller allows us to logout
-     *
-     * @return void
-     */
-
-    #[Route('/deconnexion', name: 'security.logout')]
-    public function logout() {
-        // Nothing to do here
-    }
-
-    /**
-     * This controller allows us to register 
-     *
-     * @param Request $request
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-
-     #[Route('/inscription', 'security.registration', methods: ['GET', 'POST'])]
-    public function registration(Request $request, EntityManagerInterface $manager, ScheduleRepository $scheduleRepository): Response
+    #[Route(path: '/connexion', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils,
+    ScheduleRepository $scheduleRepository): Response
     {
-        $user = new User();
-        $user->setRoles(['ROLE_USER']);
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
 
-        $form = $this->createForm(RegistrationType::class, $user);
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            $user = $form->getData();
-
-            $this->addFlash(
-                'success',
-                'Votre compte a bien été créé.'
-            );
-
-            $manager->persist($user);
-            $manager->flush();
-
-            return $this->redirectToRoute('security.login');
-        }
-
-        return $this->render('pages/security/registration.html.twig', [
-            'form' => $form->createView(), 
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername, 
+            'error' => $error,
             'schedules' => $scheduleRepository->findAll()
         ]);
     }
- }
+
+    #[Route(path: '/deconnexion', name: 'app_logout')]
+    public function logout(): Response {
+        return $this->render('index.html.twig');
+    }
+
+}
